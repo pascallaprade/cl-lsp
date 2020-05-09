48fe1b28d389d4bc00cdc556292e3f2125d79b0a
Bo Yao <bo@nearprotocol.com>
2020-05-08 23:40:49 -0700

    ignore lsp errors

--- main.lisp
+++ main.lisp
@@ -8,9 +8,10 @@
 (in-package :cl-lsp/main)
 
 (defun run-tcp-mode (&key (port 10003))
-  (with-log-stream (*error-output*)
-    (log-format "server-listen~%mode:tcp~%port:~D~%" port)
-    (jsonrpc:server-listen *server* :port port :mode :tcp)))
+  (with-open-stream (*error-output* (make-broadcast-stream))
+     (with-log-file ("~/lsp-log")
+      (log-format "server-listen~%mode:tcp~%port:~D~%" port)
+      (jsonrpc:server-listen *server* :port port :mode :tcp))))
 
 (defun run-stdio-mode ()
   (with-log-file ("~/lsp-log")
